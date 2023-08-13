39db0798d7c2248cc5083693b48196b32b48ee50
alamin-karno <amin15-1951@diu.edu.bd>
2023-08-13 16:32:28 +0600

    Initial Commit

---
+++ Trackizer/TrackizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackizerApp.swift
+//  Trackizer
+//
+//  Created by OnnoRokom on 13/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TrackizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
